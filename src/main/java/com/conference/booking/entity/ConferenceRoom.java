60903cafa58fbc615b3d9540aea919f5fc0bd60a
AbhishekDub <AbhishekDub@mashreq.com>
2024-01-21 16:47:57 +0400

    Setting default value

--- src/main/java/com/conference/booking/entity/ConferenceRoom.java
+++ src/main/java/com/conference/booking/entity/ConferenceRoom.java
@@ -1,5 +1,6 @@
 package com.conference.booking.entity;
 
+import java.util.Objects;
 import java.util.Set;
 import lombok.*;
 @NoArgsConstructor
@@ -10,5 +11,18 @@ public class ConferenceRoom {
     private String name;
     private int capacity;
     private Set<String> maintenanceTimings;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ConferenceRoom that = (ConferenceRoom) o;
+        return capacity == that.capacity && Objects.equals(name, that.name) && Objects.equals(maintenanceTimings, that.maintenanceTimings);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, capacity, maintenanceTimings);
+    }
 }
 
